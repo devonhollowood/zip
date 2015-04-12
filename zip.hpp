f90e53e2063f74edffbf707551356d09f32fee3d
Devon Hollowood <devonhollowood@gmail.com>
2015-04-12 10:55:32 -0400

    Renamed zipIterator to zip_iterator, in line with STL conventions

--- zip.hpp
+++ zip.hpp
@@ -6,11 +6,11 @@
 #include <iterator>
 
 template<typename Iterator, typename... Iterators>
-class zipIterator;
+class zip_iterator;
 
 //recursive case
 template<typename IteratorA, typename IteratorB, typename... Iterators>
-class zipIterator<IteratorA, IteratorB, Iterators...>
+class zip_iterator<IteratorA, IteratorB, Iterators...>
     : public std::iterator<
         std::forward_iterator_tag,
         decltype(std::tuple_cat(
@@ -19,18 +19,18 @@ class zipIterator<IteratorA, IteratorB, Iterators...>
                     ::value_type>
             >(),
             std::declval<
-                typename std::iterator_traits<zipIterator<IteratorB,
+                typename std::iterator_traits<zip_iterator<IteratorB,
                     Iterators...>>::value_type
             >()
         ))
     >{
 private:
     IteratorA itr_;
-    zipIterator<IteratorB, Iterators...> rest_itr_;
+    zip_iterator<IteratorB, Iterators...> rest_itr_;
 public:
-    zipIterator(IteratorA itr_a, IteratorB itr_b, Iterators... itrs)
+    zip_iterator(IteratorA itr_a, IteratorB itr_b, Iterators... itrs)
         : itr_(itr_a),
-            rest_itr_(zipIterator<IteratorB, Iterators...>(itr_b, itrs...)) {}
+            rest_itr_(zip_iterator<IteratorB, Iterators...>(itr_b, itrs...)) {}
     auto operator*() -> decltype(std::tuple_cat(std::tie(*itr_), *rest_itr_)){
         return std::tuple_cat(std::tie(*itr_), *rest_itr_);
     }
@@ -38,25 +38,25 @@ public:
         ++itr_;
         ++rest_itr_;
     }
-    zipIterator<IteratorA, IteratorB, Iterators...> operator++(int){
-        zipIterator<IteratorA, IteratorB, Iterators...> tmp(*this);
+    zip_iterator<IteratorA, IteratorB, Iterators...> operator++(int){
+        zip_iterator<IteratorA, IteratorB, Iterators...> tmp(*this);
         ++itr_;
         ++rest_itr_;
         return tmp;}
-    friend bool operator==(zipIterator<IteratorA, IteratorB, Iterators...> a,
-            zipIterator<IteratorA, IteratorB, Iterators...> b){
+    friend bool operator==(zip_iterator<IteratorA, IteratorB, Iterators...> a,
+            zip_iterator<IteratorA, IteratorB, Iterators...> b){
         return a.itr_==b.itr_ || a.rest_itr_==b.rest_itr_;
     }
 
-    friend bool operator!=(zipIterator<IteratorA, IteratorB, Iterators...> a,
-            zipIterator<IteratorA, IteratorB, Iterators...> b){
+    friend bool operator!=(zip_iterator<IteratorA, IteratorB, Iterators...> a,
+            zip_iterator<IteratorA, IteratorB, Iterators...> b){
         return a.itr_!=b.itr_ && a.rest_itr_!=b.rest_itr_;
     }
 };
 
 //base case
 template<typename Iterator>
-class zipIterator<Iterator>
+class zip_iterator<Iterator>
     : public std::iterator<
         std::forward_iterator_tag,
         std::tuple<typename std::iterator_traits<Iterator>::value_type>
@@ -64,20 +64,20 @@ class zipIterator<Iterator>
 private:
     Iterator itr_;
 public:
-    zipIterator(Iterator itr) : itr_(itr) {}
+    zip_iterator(Iterator itr) : itr_(itr) {}
     auto operator*() -> decltype(std::tie(*itr_)){
         return std::tie(*itr_);
     }
     void operator++(){++itr_;}
-    zipIterator<Iterator> operator++(int){
-        zipIterator<Iterator> tmp(*this);
+    zip_iterator<Iterator> operator++(int){
+        zip_iterator<Iterator> tmp(*this);
         ++itr_;
         return tmp;}
-    friend bool operator==(zipIterator<Iterator> a, zipIterator<Iterator> b){
+    friend bool operator==(zip_iterator<Iterator> a, zip_iterator<Iterator> b){
         return a.itr_==b.itr_;
     }
 
-    friend bool operator!=(zipIterator<Iterator> a, zipIterator<Iterator> b){
+    friend bool operator!=(zip_iterator<Iterator> a, zip_iterator<Iterator> b){
         return a.itr_!=b.itr_;
     }
 };
@@ -86,7 +86,7 @@ public:
 template<typename... Containers>
 class ZipContainer{
 public:
-    using iterator = zipIterator<
+    using iterator = zip_iterator<
         decltype(std::declval<Containers>().begin())...>;
     ZipContainer(Containers&... cs) : begin_(iterator(std::begin(cs)...)),
         end_(iterator(std::end(cs)...)) {};
--- ziptest.cpp
+++ ziptest.cpp
@@ -9,16 +9,16 @@ BOOST_AUTO_TEST_SUITE(ZipIterator)
 
 BOOST_AUTO_TEST_CASE(constructor_test) {
     vector<int> v = {0,1,2};
-    zipIterator<vector<int>::iterator> itr_a(v.begin());
-    zipIterator<vector<int>::iterator> itr_b(itr_a);
-    zipIterator<vector<int>::iterator> itr_c = itr_a;
+    zip_iterator<vector<int>::iterator> itr_a(v.begin());
+    zip_iterator<vector<int>::iterator> itr_b(itr_a);
+    zip_iterator<vector<int>::iterator> itr_c = itr_a;
     BOOST_CHECK(itr_b==itr_c);
 }
 
 BOOST_AUTO_TEST_CASE(equality_test) {
     vector<int> v = {0,1,2};
-    zipIterator<vector<int>::iterator> itr_a(v.begin());
-    zipIterator<vector<int>::iterator> itr_b(v.begin());
+    zip_iterator<vector<int>::iterator> itr_a(v.begin());
+    zip_iterator<vector<int>::iterator> itr_b(v.begin());
     BOOST_CHECK(itr_a==itr_b);
     ++itr_a;
     BOOST_CHECK(itr_a!=itr_b);
@@ -28,14 +28,14 @@ BOOST_AUTO_TEST_CASE(equality_test) {
 
 BOOST_AUTO_TEST_CASE(rvalue_deref_test) {
     vector<int> v = {0};
-    zipIterator<vector<int>::iterator> itr(v.begin());
+    zip_iterator<vector<int>::iterator> itr(v.begin());
     int res = std::get<0>(*itr);
     BOOST_CHECK_EQUAL(res, 0);
 }
 
 BOOST_AUTO_TEST_CASE(lvalue_deref_test) {
     vector<int> v = {0,3};
-    zipIterator<vector<int>::iterator> itr(v.begin());
+    zip_iterator<vector<int>::iterator> itr(v.begin());
     BOOST_CHECK(*itr==std::make_tuple(0));
     *itr = std::make_tuple(1);
     //v is {1,3}
@@ -49,7 +49,7 @@ BOOST_AUTO_TEST_CASE(lvalue_deref_test) {
 
 BOOST_AUTO_TEST_CASE(increment_test){
     vector<int> v = {0,1,2,3};
-    zipIterator<vector<int>::iterator> itr(v.begin());
+    zip_iterator<vector<int>::iterator> itr(v.begin());
     BOOST_CHECK_EQUAL(std::get<0>(*itr), 0);
     ++itr;
     BOOST_CHECK_EQUAL(std::get<0>(*itr), 1);
@@ -61,8 +61,8 @@ BOOST_AUTO_TEST_CASE(increment_test){
 
 BOOST_AUTO_TEST_CASE(iteration_test) {
     vector<int> v = {0,1,2,3,4};
-    zipIterator<vector<int>::iterator> itr(v.begin());
-    zipIterator<vector<int>::iterator> end(v.end());
+    zip_iterator<vector<int>::iterator> itr(v.begin());
+    zip_iterator<vector<int>::iterator> end(v.end());
     size_t i = 0;
     for(; itr!=end; ++itr, ++i){
         if(i>=v.size()){
@@ -75,8 +75,8 @@ BOOST_AUTO_TEST_CASE(iteration_test) {
 
 BOOST_AUTO_TEST_CASE(swap_test) {
     vector<int> v = {0,1};
-    zipIterator<vector<int>::iterator> itr_a(v.begin());
-    zipIterator<vector<int>::iterator> itr_b(++v.begin());
+    zip_iterator<vector<int>::iterator> itr_a(v.begin());
+    zip_iterator<vector<int>::iterator> itr_b(++v.begin());
     BOOST_CHECK_EQUAL(std::get<0>(*itr_a), 0);
     BOOST_CHECK_EQUAL(std::get<0>(*itr_b), 1);
     std::swap(itr_a, itr_b);
@@ -93,19 +93,19 @@ BOOST_AUTO_TEST_SUITE(MultiZipIterator)
 BOOST_AUTO_TEST_CASE(constructor_test) {
     vector<int> v1 = {0,1,2};
     vector<char> v2 = {'a','b','c'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr_a(v1.begin(), v2.begin());
-    zipIterator<vector<int>::iterator, vector<char>::iterator> itr_b(itr_a);
-    zipIterator<vector<int>::iterator, vector<char>::iterator> itr_c = itr_a;
+    zip_iterator<vector<int>::iterator, vector<char>::iterator> itr_b(itr_a);
+    zip_iterator<vector<int>::iterator, vector<char>::iterator> itr_c = itr_a;
     BOOST_CHECK(itr_b==itr_c);
 }
 
 BOOST_AUTO_TEST_CASE(equality_test) {
     vector<int> v1 = {0,1,2};
     vector<char> v2 = {'a','b','c'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr_a(v1.begin(), v2.begin());
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr_b(v1.begin(), v2.begin());
     BOOST_CHECK(itr_a==itr_b);
     ++itr_a;
@@ -117,7 +117,7 @@ BOOST_AUTO_TEST_CASE(equality_test) {
 BOOST_AUTO_TEST_CASE(rvalue_deref_test) {
     vector<int> v1 = {0};
     vector<char> v2 = {'a'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr(v1.begin(), v2.begin());
     int res1 = std::get<0>(*itr);
     char res2 = std::get<1>(*itr);
@@ -128,7 +128,7 @@ BOOST_AUTO_TEST_CASE(rvalue_deref_test) {
 BOOST_AUTO_TEST_CASE(lvalue_deref_test) {
     vector<int> v1 = {0,3};
     vector<char> v2 = {'a','d'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr(v1.begin(), v2.begin());
     BOOST_CHECK(*itr==std::make_tuple(0, 'a'));
     *itr = std::make_tuple(1,'b');
@@ -148,7 +148,7 @@ BOOST_AUTO_TEST_CASE(lvalue_deref_test) {
 BOOST_AUTO_TEST_CASE(increment_test){
     vector<int> v1 = {0, 1, 2, 3};
     vector<char> v2 = {'a', 'b', 'c', 'd'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr(v1.begin(), v2.begin());
     BOOST_CHECK_EQUAL(std::get<0>(*itr), 0);
     BOOST_CHECK_EQUAL(std::get<1>(*itr), 'a');
@@ -166,9 +166,9 @@ BOOST_AUTO_TEST_CASE(increment_test){
 BOOST_AUTO_TEST_CASE(iteration_test) {
     vector<int> v1 = {0, 1, 2, 3};
     vector<char> v2 = {'a', 'b', 'c'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr(v1.begin(), v2.begin());
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         end(v1.end(), v2.end());
     size_t i = 0;
     for(; itr!=end; ++itr, ++i){
@@ -184,9 +184,9 @@ BOOST_AUTO_TEST_CASE(iteration_test) {
 BOOST_AUTO_TEST_CASE(swap_test) {
     vector<int> v1 = {0, 1};
     vector<char> v2 = {'a', 'b'};
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr_a(v1.begin(), v2.begin());
-    zipIterator<vector<int>::iterator, vector<char>::iterator>
+    zip_iterator<vector<int>::iterator, vector<char>::iterator>
         itr_b(++v1.begin(), ++v2.begin());
     BOOST_CHECK_EQUAL(std::get<0>(*itr_a), 0);
     BOOST_CHECK_EQUAL(std::get<1>(*itr_a), 'a');
